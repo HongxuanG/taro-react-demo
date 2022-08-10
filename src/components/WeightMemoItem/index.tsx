9026d81faa7b6b98e862b4c1685e29db8b5d054c
HongxuanG <1359774872@qq.com>
2022-08-10 23:59:34 +0800

    ✨ feat(memo): 学习memo

--- src/components/WeightMemoItem/index.tsx
+++ src/components/WeightMemoItem/index.tsx
@@ -1,21 +1,21 @@
 import { View, Image } from '@tarojs/components'
+import React from 'react'
+import styles from './index.module.scss'
 
 type PropsType = {
   url: string
   title: string
   desc: string
 }
-const WeightMemoItem = ({ url, title, desc }: PropsType) => {
+const WeightMemoItem: React.FC<PropsType> = ({ url, title, desc }) => {
   return (
-    <View className='ul-container'>
-      <View className='li-container'>
-        <View className='leftSlot'>
-          <Image src={url}></Image>
-        </View>
-        <View className='rightSlot'>
-          <View className='title'>{title}</View>
-          <View className='desc'>{desc}</View>
-        </View>
+    <View className={styles['item-container']}>
+      <View className={styles['item-image']}>
+        <Image className='image-full' src={url}></Image>
+      </View>
+      <View className={styles['item-context']}>
+        <View className={styles['item-title']}>{title}</View>
+        <View className={styles['item-desc']}>{desc}</View>
       </View>
     </View>
   )
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -5,18 +5,70 @@ import { useState } from 'react'
 import './index.module.scss'
 
 const Index = () => {
-  const [checked, setChecked] = useState(false)
-  const handleCheckboxChange = (isCheck: boolean) => {
-    setChecked(isCheck)
+  const [weightList, setWeightList] = useState(() => {
+    return [
+      {
+        id: 1,
+        checked: false,
+        url: 'https://s1.ax1x.com/2022/07/03/j8Tusx.png',
+        title: 'vue3手写源码1',
+        desc: '欢迎学习vue3源码'
+      },
+      {
+        id: 2,
+        checked: false,
+        url: 'https://s1.ax1x.com/2022/07/03/j8Tusx.png',
+        title: 'vue3手写源码2',
+        desc: '欢迎学习vue3源码2'
+      },
+      {
+        id: 3,
+        checked: false,
+        url: 'https://s1.ax1x.com/2022/07/03/j8Tusx.png',
+        title: 'vue3手写源码3',
+        desc: '欢迎学习vue3源码3'
+      },
+      {
+        id: 4,
+        checked: false,
+        url: 'https://s1.ax1x.com/2022/07/03/j8Tusx.png',
+        title: 'vue3手写源码4',
+        desc: '欢迎学习vue3源码4'
+      },
+      {
+        id: 5,
+        checked: false,
+        url: 'https://s1.ax1x.com/2022/07/03/j8Tusx.png',
+        title: 'vue3手写源码5',
+        desc: '欢迎学习vue3源码5'
+      }
+    ]
+  })
+  type WeightListItem = typeof weightList[number]
+  const handleCheckboxChange = (id: number, isCheck: boolean) => {
+    console.log('isCheck==>', isCheck)
+    let newList: WeightListItem[] = []
+    weightList.map((item: WeightListItem) => {
+      if (item.id === id) {
+        item.checked = isCheck
+      }
+      newList.push(item)
+    })
+    setWeightList(newList)
   }
   return (
     <View>
-      <Checkbox checked={checked} size='normal' onChange={handleCheckboxChange}></Checkbox>
-      <WeightMemoItem
-        url='https://s1.ax1x.com/2022/07/03/j8Tusx.png'
-        title='Diff算法的图解'
-        desc='欢迎来学习vue3的diff算法'
-      />
+      {weightList.map(({ checked, url, desc, title, id }) => {
+        return (
+          <Checkbox
+            checked={checked}
+            size='normal'
+            onChange={isCheck => handleCheckboxChange(id, isCheck)}
+          >
+            <WeightMemoItem url={url} title={title} desc={desc} />
+          </Checkbox>
+        )
+      })}
     </View>
   )
 }
